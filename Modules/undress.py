d56151ddea445a6c5c8c4e9680434257230adea4
Mechazawa <BasBieling@gmail.com>
2014-01-18 16:49:58 +0100

    Update undress.py

--- Modules/undress.py
+++ Modules/undress.py
@@ -5,4 +5,4 @@ from BotKit import command, stylize
 
 @command("undress")
 def undress(bot, channel, user, msg):
-    bot.msg(channel, "Current code: https://github.com/Mechazawa/Nimdok " + stylize.SetColor("HEAVY WIP!", stylize.Color.Red))
+    bot.msg(channel, "Current code: https://github.com/Mechazawa/Nimdok")
