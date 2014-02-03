ca86ef780223d7f1623c40b4153b1560b00e558c
pruf <proof.romania@gmail.com>
2014-02-03 16:48:28 +0200

    nimdok is love nimdok is life

---
+++ Modules/yousuck.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+from BotKit import *
+import random
+
+replies = [ 'no u', 'your mother is so fat, the recursive function computing her mass causes a stack overflow' ]
+insults = [ 'blows', 'is an ass', 'is broken', 'is crap', 'is shit', 'sucks' ]
+
+@handles('msg')
+def parse(bot, channel, user, msg):
+    if msg.lower().strip().split(' ')[0] == 'nimdok' and msg.lower().replace('nimdok', '').strip() in insults:
+        bot.msg(channel, user+': '+random.choice(replies)) 
+
