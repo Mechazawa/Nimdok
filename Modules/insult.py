03aa6970a97f43b1f97571a758918fb4861f4658
Mechazawa <mega@ioexception.at>
2014-02-04 08:42:44 +0100

    lol github

--- Modules/insult.py
+++ Modules/insult.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
-# Made by psycho
-
-from BotKit import handles
+from BotKit import *
 import random
-
-replies = [ 'no u', 'your mother is so fat, the recursive function computing her mass causes a stack overflow' ]
-insults = [ 'blows', 'is an ass', 'is broken', 'is crap', 'is shit', 'sucks' ]
 
 @handles('msg')
 def parse(bot, channel, user, msg):
     if msg.lower().strip().split(' ')[0] == 'nimdok' and msg.lower().replace('nimdok', '').strip() in insults:
-        bot.msg(channel, user+': '+random.choice(replies)) 
+        insults = [ 'blows', 'is an ass', 'is broken', 'is crap', 'is shit', 'sucks' ]
+        replies = open('insults.in', 'r')
+        bot.msg(channel, user+': '+random.choice(replies.readlines()).strip()) 
+
+@command('insult')
+def parse(bot, channel, user, msg):
+    replies = open('insults.in', 'w')
+    replies.write(msg + '\n')
