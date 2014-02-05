dfcc8577d80047fb014955a2d770ef944b755739
Mechazawa <Mega@IOException.at>
2014-02-05 23:11:29 +0100

    Admin module

---
+++ Modules/admin.py
@@ -0,0 +1,27 @@
+from BotKit import admin, command
+
+@command('addadmin', True)
+def parse(bot, channel, user, args):
+    usr =args.split()[0]
+    if admin().isadmin(usr):
+        bot.msg(channel, "%s: %s is already an admin" % (user, usr))
+    else:
+        admin().add(usr)
+        bot.msg(channel, "%s: I added %s to the admin group" % (user, usr))
+
+
+@command('admins')
+def parse(bot, channel, user, args):
+    bot.msg(channel, "%s: %s" %(user, ', '.join(admin().getadmins())))
+
+@command('remadmin', True)
+def parse(bot, channel, user, args):
+    usr =args.split()[0]
+    if usr.lower() == user.lower():
+        bot.msg(channel, 'lol okay');
+
+    if admin().isadmin(usr):
+        admin().remove(usr)
+        bot.msg(channel, "%s: I removed %s from the admin group" % (user, usr))
+    else:
+        bot.msg(channel, "%s: %s was never an admin" % (user, usr))
