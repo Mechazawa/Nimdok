9a4c3a34d010cb9e6a3e93af7b05f5edc370701c
pruf <proof.romania@gmail.com>
2014-02-07 13:39:01 +0200

    cats on keyboards

--- Modules/weather.py
+++ Modules/weather.py
@@ -29,41 +29,45 @@ def parse(bot, channel, user, arg):
                 .replace('<relative_humidity>','').replace('</relative_humidity>','')
         wind = data.getElementsByTagName('wind_string')[0].toxml() \
                 .replace('<wind_string>','').replace('</wind_string>','') \
-                .replace('F', 'f')
-
-        if 'MPH' in wind:
-            mph = wind.split(' ')[len(wind.split(' '))-3]
-            kmph = round(float(mph) * 1.609, 1)
-            wind = wind.replace(str(mph), str(kmph)).replace('MPH', 'km/h')
-
-        tempColor = ''
-        if '-' in temp:
-            tmp = float(temp[1:])
-            if tmp >= 0 and tmp < 3:
-                tempColor = stylize.Color.LCyan
-            elif tmp >= 3 and tmp < 8:
-                tempColor = stylize.Color.Cyan 
-            elif tmp >= 8 and tmp < 15:
-                tempColor = stylize.Color.LBlue 
-            elif tmp >= 15:
-                tempColor = stylize.Color.Blue 
-        else:
-            tmp = float(temp)
-            if tmp >= 0 and tmp < 3:
-                tempColor = stylize.Color.White
-            elif tmp >= 3 and tmp < 10:
-                tempColor = stylize.Color.Green 
-            elif tmp >= 10 and tmp < 25:
-                tempColor = stylize.Color.Yellow 
-            elif tmp >= 25 and tmp < 35:
-                tempColor = stylize.Color.Orange 
-            elif tmp >= 35:
-                tempColor = stylize.Color.Red 
+                .replace('F', 'f').replace('MPH', 'km/h')
 
         if location != ', ':
+            for i in wind.split():
+                try:
+                    int(i)
+                except:
+                    pass
+                else:
+                    kmph = round(float(i)*1.609, 1)
+                    wind = wind.replace(i, str(kmph))
+
+            tempColor = ''
+            if '-' in temp:
+                tmp = float(temp[1:])
+                if tmp >= 0 and tmp < 3:
+                    tempColor = stylize.Color.LCyan
+                elif tmp >= 3 and tmp < 8:
+                    tempColor = stylize.Color.Cyan 
+                elif tmp >= 8 and tmp < 15:
+                    tempColor = stylize.Color.LBlue 
+                elif tmp >= 15:
+                    tempColor = stylize.Color.Blue 
+            else:
+                tmp = float(temp)
+                if tmp >= 0 and tmp < 3:
+                    tempColor = stylize.Color.White
+                elif tmp >= 3 and tmp < 10:
+                    tempColor = stylize.Color.Green 
+                elif tmp >= 10 and tmp < 25:
+                    tempColor = stylize.Color.Yellow 
+                elif tmp >= 25 and tmp < 35:
+                    tempColor = stylize.Color.Orange 
+                elif tmp >= 35:
+                    tempColor = stylize.Color.Red 
+
             bot.msg(channel, user + ': ' + location + ', ' + 
                             stylize.SetColor(temp + 'C', tempColor) + 
-                            '. Wind blows ' + 
+                            ', ' + weather + '. Wind blows ' + 
                             wind.strip() + ', ' + humid + ' humidity.')
         else:
             bot.msg(channel, user + ': I couldn\'t find the location.')
