104cc35c838586a324dfddcdb455687545bb9f87
pruf <proof.romania@gmail.com>
2014-02-09 15:44:52 +0200

    fixed 404 and other

--- Modules/urlinfo.py
+++ Modules/urlinfo.py
@@ -19,8 +19,12 @@ def parse(bot, channel, user, msg):
             try: 
                 head = urllib2.urlopen(HeadRequest(m))
             except: 
-                source = urllib2.urlopen(m).read().replace('\r','').replace('\n','')
-                bot.msg(channel, stylize.Trunicate(source))
+                try:
+                    source = urllib2.urlopen(m).read().replace('\r','').replace('\n','')
+                except Exception, e:
+                    bot.msg(channel, e)
+                else:
+                    bot.msg(channel, stylize.Trunicate(source))
             else:
                 head.read()
                 mimetype = head.headers.type
