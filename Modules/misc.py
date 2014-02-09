97d7d6b7e9fa77d1dd662a020604f6d8fced00be
Mechazawa <BasBieling@gmail.com>
2014-02-09 12:20:59 +0100

    fork that dongle

---
+++ Modules/misc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from BotKit import command
+
+@command("dongle")
+def fork(bot, channel, user, arg):
+  forkable = arg.split()[0] if arg.split()[0] else user
+  bot.action(channel, "forks %s'%s dongle" % (forkable, "s" if forkable[-1].lower() not in "zxs" else ""))
