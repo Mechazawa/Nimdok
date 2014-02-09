9b5e182dbdcb9eaddf42e6966a80f5fc262aef83
Mechazawa <BasBieling@gmail.com>
2014-02-09 12:23:39 +0100

    Update misc.py

--- Modules/misc.py
+++ Modules/misc.py
@@ -4,5 +4,5 @@ from BotKit import command
 
 @command("dongle")
 def fork(bot, channel, user, arg):
-  forkable = arg.split()[0] if arg.split()[0] else user
+  forkable = arg.split()[0] if len(arg.split()) else user
   bot.action(channel, "forks %s'%s dongle" % (forkable, "s" if forkable[-1].lower() not in "zxs" else ""))
