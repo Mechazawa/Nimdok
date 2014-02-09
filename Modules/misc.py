63bfa8705ec450d38c7040eb618ec20cd9fcbd85
Mechazawa <BasBieling@gmail.com>
2014-02-09 12:27:14 +0100

    Update misc.py

--- Modules/misc.py
+++ Modules/misc.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from BotKit import command
+from BotKit import command, handles
 
 @command("dongle")
 def fork(bot, channel, user, arg):
-  forkable = arg.split()[0] if len(arg.split()) else user
-  bot.action(channel, "forks %s'%s dongle" % (forkable, "s" if forkable[-1].lower() not in "zxs" else ""))
+    forkable = arg.split()[0] if len(arg.split()) else user
+    bot.action(channel, "forks %s'%s dongle" % (forkable, "s" if forkable[-1].lower() not in "zxs" else ""))
+
+@handles("msg")
+def dongers(bot, channel, user, msg):
+    if "donger" in msg.lower():
+        bot.msg(channel, u"ヽ༼ຈل͜ຈ༽ﾉ raise ur dongers ヽ༼ຈل͜ຈ༽ﾉ")
