a7b295bf620dd61da9b385137185a55119dd20a5
Mechazawa <BasBieling@gmail.com>
2014-02-10 09:20:48 +0100

    Update more.py

--- Modules/more.py
+++ Modules/more.py
@@ -7,8 +7,11 @@ from BotKit import command
 
 @command("more")
 def parse(bot, channel, user, arg):
-    more = bot.GetMore()
-    if more.strip():
-        bot.msg(channel, "%s: %s" % (user, urllib2.urlopen("http://nnmm.nl/", more).read()))
+    more = bot.GetMore().strip().replace("\r", "")
+    if more:
+        if more.count("\n") > 3 or len(more) > 1000:
+            bot.msg(channel, "%s: %s" % (user, urllib2.urlopen("http://nnmm.nl/", more).read()))
+        else:
+            bot.msg(channel, more)
     else:
         bot.msg(channel, "There is nothing more :/")
