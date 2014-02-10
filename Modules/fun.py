3e5343b52e65068b7a1628de3b5fd7776b08f626
pruf <proof.romania@gmail.com>
2014-02-10 16:04:06 +0200

    updated usage on 8ball

--- Modules/fun.py
+++ Modules/fun.py
@@ -5,9 +5,12 @@ import random
 
 @command('8ball')
 def parse(bot, channel, user, msg):
-    answers = ['yes', 'no', 'without a doubt', 'as i see it, yes', 'ask me again later', 
-               'don\'t count on it', 'my sources say no', 'very doubtful', 'don\'t care, go away']
-    bot.msg(channel, user + ': ' + random.choice(answers) + '.')
+    if len(msg) == 0:
+        bot.msg(channel, 'Usage: :8ball <question>')
+    else:
+        answers = ['yes', 'no', 'without a doubt', 'as i see it, yes', 'ask me again later', 
+                   'don\'t count on it', 'my sources say no', 'very doubtful', 'don\'t care, go away']
+        bot.msg(channel, user + ': ' + random.choice(answers) + '.')
 
 @handles('msg')
 def parse(bot, channel, user, msg):
