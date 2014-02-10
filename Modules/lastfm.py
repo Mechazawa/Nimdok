bd0cc83e8448fd58095e6bfb2ece42fee2486871
pruf <proof.romania@gmail.com>
2014-02-10 18:46:07 +0200

    updated album and tags

--- Modules/lastfm.py
+++ Modules/lastfm.py
@@ -10,6 +10,7 @@ import apikeys
 
 
 apiurl="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={USER}&api_key={APIKEY}&limit=1"
+infourl = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={APIKEY}&mbid={MBID}'
 dbfile="dbs/lastfm.db"
 @command("np")
 def parse(bot, channel, user, msg):
@@ -23,37 +24,64 @@ def parse(bot, channel, user, msg):
           if not fmuser:
               bot.msg(channel, "%s: I don't know you. Use :np register [lastfm nickname]" % user)
           else:
-              #Curtosy of psycho
-              url = apiurl.replace('{USER}', fmuser).replace('{APIKEY}', apikeys.lastfm)
-              data = urllib2.urlopen(url).read()
-              dom = parseString(data)
-
-              artistxml = dom.getElementsByTagName('artist')[0].toxml()
-              trackxml = dom.getElementsByTagName('name')[0].toxml()
-
-              artist = artistxml.rsplit('>')[1].replace('</artist','').strip().replace('\n', '')
-              track = trackxml.rsplit('>')[1].replace('</name','').strip().replace('\n', '')
-
-              state = "last heard"
+              #Courtesy of psycho
+              url = urllib2.urlopen(apiurl.replace('{USER}', fmuser).replace('{APIKEY}', apikeys.lastfm)).read()
+              data = parseString(url)
+              artist = data.getElementsByTagName('artist')[0].toxml() \
+                        .rsplit('>')[1].replace('</artist', '') \
+                        .replace('&amp;', '&')
+              track = data.getElementsByTagName('name')[0].toxml() \
+                        .rsplit('>')[1].replace('</name', '') \
+                        .replace('&amp;', '&')
+              mbid = data.getElementsByTagName('mbid')[0].toxml() \
+                        .rsplit('>')[1].replace('</mbid', '')
               try:
-                  data.index('nowplaying')
+                  tinfo = parseString(urllib2.urlopen(infourl.replace('{MBID}', mbid).replace('{APIKEY}', apikeys.lastfm)).read())
               except:
                   pass
               else:
-                  state = "is now playing"
-              bot.msg(channel, "%s %s %s - %s" % (
-                  stylize.Bold(user), state,
-                  stylize.Bold(stylize.Trunicate(artist, 100)),
-                  stylize.Bold(stylize.Trunicate(track, 100))
-              ))
+                  genres = []
+                  for i in range(2,5):
+                       genres.append(tinfo.getElementsByTagName('name')[i].toxml() \
+                             .rsplit('>')[1].replace('</name', ''))
+                  genre = ''
+                  for i in genres:
+                      genre += i + ', '
+                  album = tinfo.getElementsByTagName('title')[0].toxml() \
+                             .rsplit('>')[1].replace('</title', '')
+
+                  state = 'last heard'
+                  try:
+                      url.index('nowplaying')
+                  except:
+                      pass
+                  else:
+                      state = 'now playing'
+                  try:
+                      album
+                  except:
+                      bot.msg(channel, stylize.Bold(user) + ' ' + state + ' ' + \
+                                        stylize.Bold(stylize.Trunicate(artist, 45)) + ' - ' + \
+                                        stylize.Bold(stylize.Trunicate(track, 65)))
+                  else:
+                      bot.msg(channel, stylize.Bold(user) + ' ' + state + ' ' + \
+                                        stylize.Bold(stylize.Trunicate(artist, 45)) + ' - ' + \
+                                        stylize.Bold(stylize.Trunicate(track, 65)) + ' (Album: ' + \
+                                        album + '; Tags: ' + genre.strip()[:-1] + ')')
+
     elif s[0].lower() == "register":
         if len(s) != 2:
             bot.msg(channel, "%s: Usage, :np register [last fm nickname]" % user)
         else:
             nick = s[1].strip()
             url = "http://ws.audioscrobbler.com/1.0/user/%s/recenttracks.rss?limit=1&format=txt" % nick
-            print url
-            raw = urllib2.urlopen(url).read()
+            #print url # what does this do
+            try:
+                urllib2.urlopen(url).read()
+            except:
+                raw = 'No user exists with this name.'
+            else:
+                raw = urllib2.urlopen(url).read()
             if not "No user exists with this name." in raw:
                 cursor.execute("DELETE FROM lastfm WHERE nick = ?", (user, ))
                 cursor.execute("INSERT INTO lastfm(nick, lastfm) VALUES (?, ?)", (user, nick))
