6eaad50a40c2e527bc73ac61dc889253e82d6b48
pruf <proof.romania@gmail.com>
2014-02-20 21:32:31 +0200

    moved to json

--- Modules/lastfm.py
+++ Modules/lastfm.py
@@ -2,73 +2,65 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
+import json
 import sqlite3
 import os
-from xml.dom.minidom import parseString
 from BotKit import stylize, command
 import apikeys
 
 
-apiurl="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={USER}&api_key={APIKEY}&limit=1"
-infourl = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={APIKEY}&mbid={MBID}'
+apiurl="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={USER}&api_key={APIKEY}&format=json&limit=1"
+infourl = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={APIKEY}&format=json&mbid={MBID}'
 dbfile="dbs/lastfm.db"
-@command("np")
+@command("n")
 def parse(bot, channel, user, msg):
     s = msg.split()
     database = sqlite3.connect(dbfile)
     cursor = database.cursor()
     if len(s) == 0:
-          fmuser = False
-          for row in cursor.execute("SELECT lastfm FROM lastfm WHERE nick=?", (user, )):
-              fmuser=row[0]
-          if not fmuser:
-              bot.msg(channel, "%s: I don't know you. Use :np register [lastfm nickname]" % user)
-          else:
-              #Courtesy of psycho
-              url = urllib2.urlopen(apiurl.replace('{USER}', fmuser).replace('{APIKEY}', apikeys.lastfm)).read()
-              data = parseString(url)
-              artist = data.getElementsByTagName('artist')[0].toxml() \
-                        .rsplit('>')[1].replace('</artist', '') \
-                        .replace('&amp;', '&')
-              track = data.getElementsByTagName('name')[0].toxml() \
-                        .rsplit('>')[1].replace('</name', '') \
-                        .replace('&amp;', '&')
-              mbid = data.getElementsByTagName('mbid')[0].toxml() \
-                        .rsplit('>')[1].replace('</mbid', '')
-              try:
-                  tinfo = parseString(urllib2.urlopen(infourl.replace('{MBID}', mbid).replace('{APIKEY}', apikeys.lastfm)).read())
-              except:
-                  pass
-              else:
-                  genres = []
-                  for i in range(2,5):
-                       genres.append(tinfo.getElementsByTagName('name')[i].toxml() \
-                             .rsplit('>')[1].replace('</name', ''))
-                  genre = ''
-                  for i in genres:
-                      genre += i + ', '
-                  album = tinfo.getElementsByTagName('title')[0].toxml() \
-                             .rsplit('>')[1].replace('</title', '')
-
-                  state = 'last heard'
-                  try:
-                      url.index('nowplaying')
-                  except:
-                      pass
-                  else:
-                      state = 'now playing'
-                  try:
-                      album
-                  except:
-                      bot.msg(channel, stylize.Bold(user) + ' ' + state + ' ' + \
-                                        stylize.Bold(stylize.Trunicate(artist, 45)) + ' - ' + \
-                                        stylize.Bold(stylize.Trunicate(track, 65)))
-                  else:
-                      bot.msg(channel, stylize.Bold(user) + ' ' + state + ' ' + \
-                                        stylize.Bold(stylize.Trunicate(artist, 45)) + ' - ' + \
-                                        stylize.Bold(stylize.Trunicate(track, 65)) + ' (Album: ' + \
-                                        album + '; Tags: ' + genre.strip()[:-1] + ')')
-
+        fmuser = False
+        for row in cursor.execute("SELECT lastfm FROM lastfm WHERE nick=?", (user, )):
+            fmuser=row[0]
+        if not fmuser:
+            bot.msg(channel, "%s: I don't know you. Use :np register [lastfm nickname]" % user)
+        else:
+            url = apiurl.replace('{APIKEY}', apikeys.lastfm).replace('{USER}', fmuser)
+            data = json.load(urllib2.urlopen(url))
+            try:
+                artist = data['recenttracks']['track'][0]['artist']['#text']
+            except:
+                artist = data['recenttracks']['track']['artist']['#text']
+            else:
+                pass
+            try:
+                track = data['recenttracks']['track'][0]['name']
+            except:
+                track = data['recenttracks']['track']['name']
+            else:
+                pass
+            try:
+                album = data['recenttracks']['track'][0]['album']['#text']
+            except:
+                album = data['recenttracks']['track']['album']['#text']
+            else:
+                pass
+            state = stylize.SetColor('last heard', stylize.Color.Red)
+            try:
+                data.index('nowplaying')
+            except:
+                pass
+            else:
+                state = stylize.SetColor('now playing', stylize.Color.Green)
+            if album != '':
+                bot.msg(channel, stylize.Bold(user) + ' ' + state + ' ' + \
+                                 stylize.Bold(stylize.Trunicate(artist, 45)) + ' - ' + \
+                                 stylize.Bold(stylize.Trunicate(track, 65)) + ' (Album: ' + \
+                                 album + ')')
+            else:
+                bot.msg(channel, stylize.Bold(user) + ' ' + state + ' ' + \
+                                 stylize.Bold(stylize.Trunicate(artist, 45)) + ' - ' + \
+                                 stylize.Bold(stylize.Trunicate(track, 65)))
+             
     elif s[0].lower() == "register":
         if len(s) != 2:
             bot.msg(channel, "%s: Usage, :np register [last fm nickname]" % user)
