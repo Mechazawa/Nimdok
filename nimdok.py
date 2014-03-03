bfaf0a3da541d564143e98faf754b77fb2c43030
Mechazawa <BasBieling@gmail.com>
2014-03-03 22:10:46 +0100

    Update nimdok.py

--- nimdok.py
+++ nimdok.py
@@ -26,5 +26,10 @@ def invite(bot, channel, user):
 @handles('msg')
 def msg(bot, channel, user, msg):
     bot.logger.info("%s %s: %s" % (channel, user, msg))
+    
+@command('die', True)
+def die(bot, channel, user, msg):
+    bot.msg(channel, "o-okay then")
+    bot.quit("Suicide")
 
 irc.run()
