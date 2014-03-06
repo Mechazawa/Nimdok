89bfa8e6661b2b2a6c7a2fd25f60a40bb2abbe88
pruf <proof.romania@gmail.com>
2014-03-06 13:44:03 +0200

    now using json

--- Modules/weather.py
+++ Modules/weather.py
@@ -2,76 +2,35 @@
 #!/usr/bin/python
 from BotKit import *
 import urllib2
-from xml.dom.minidom import parseString
-
-@command('weather')
-def parse(bot, channel, user, arg):
-    if len(arg) == 0:
-        bot.msg(channel, 'Usage: :weather <city> [<country>]')
-    else:
-        try:
-            arg.split(' ')[1]
-        except:
-            loc = arg.split(' ')[0]
-        else:
-            loc = arg.split(' ')[0] + ',' + arg.split(' ')[1]
-
-        url = 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query='+loc.strip()
-        data = parseString(urllib2.urlopen(url).read())
-
-        location = data.getElementsByTagName('full')[0].toxml() \
-                .replace('<full>','').replace('</full>','')
-        temp = data.getElementsByTagName('temp_c')[0].toxml() \
-                .replace('<temp_c>','').replace('</temp_c>','')
-        weather = data.getElementsByTagName('weather')[0].toxml() \
-                .replace('<weather>','').replace('</weather>','')
-        humid = data.getElementsByTagName('relative_humidity')[0].toxml() \
-                .replace('<relative_humidity>','').replace('</relative_humidity>','')
-        wind = data.getElementsByTagName('wind_string')[0].toxml() \
-                .replace('<wind_string>','').replace('</wind_string>','') \
-                .replace('F', 'f').replace('MPH', 'km/h')
-
-        if location != ', ':
-            for i in wind.split():
-                try:
-                    int(i)
-                except:
-                    pass
-                else:
-                    kmph = round(float(i)*1.609, 1)
-                    wind = wind.replace(i, str(kmph))
-
-            tempColor = ''
-            if '-' in temp:
-                tmp = float(temp[1:])
-                if tmp >= 0 and tmp < 3:
-                    tempColor = stylize.Color.LCyan
-                elif tmp >= 3 and tmp < 8:
-                    tempColor = stylize.Color.Cyan 
-                elif tmp >= 8 and tmp < 15:
-                    tempColor = stylize.Color.LBlue 
-                elif tmp >= 15:
-                    tempColor = stylize.Color.Blue 
-            else:
-                tmp = float(temp)
-                if tmp >= 0 and tmp < 10:
-                    tempColor = stylize.Color.White
-                elif tmp >= 10 and tmp < 16:
-                    tempColor = stylize.Color.Yellow 
-                elif tmp >= 16 and tmp < 25:
-                    tempColor = stylize.Color.Green
-                elif tmp >= 25 and tmp < 35:
-                    tempColor = stylize.Color.Orange 
-                elif tmp >= 35:
-                    tempColor = stylize.Color.Red 
-
-            bot.msg(channel, user + ': ' + location + ', ' + 
-                            stylize.SetColor(temp + 'C', tempColor) + 
-                            ', ' + weather + '. Wind blows ' + 
-                            wind.strip() + ', ' + humid + ' humidity.')
-        else:
-            bot.msg(channel, user + ': I couldn\'t find the location.')
+import json
+import apikeys
 
 @command('w')
-def redirect(bot, channel, user, arg):
-    parse(bot, channel, user, arg)
+def parse(bot, channel, user, arg):
+    if len(arg) == 0:
+        bot.msg(channel, 'Usage, :w <city> [<country>]')
+    else:
+        location = urllib2.quote(arg)
+        apiurl = 'http://api.wunderground.com/api/{APIKEY}/conditions/q/{LOCATION}.json' \
+                .replace('{APIKEY}', apikeys.wunder).replace('{LOCATION}', location)
+        data = json.load(urllib2.urlopen(apiurl))
+        text = urllib2.urlopen(apiurl).read()
+        if 'current_observation' in text:
+            key = 'current_observation'
+            curr_loc = data[key]['display_location']['full']
+            weather = data[key]['weather']
+            temp = str(data[key]['temp_c']) + 'C (' + str(data[key]['temp_f']) + 'F)'
+            feel = str(data[key]['feelslike_c']) + 'C (' + str(data[key]['feelslike_f']) + 'F)'
+            humidity = data[key]['relative_humidity']
+            wkph = str(data[key]['wind_kph'])
+            wmph = str(data[key]['wind_mph'])
+            wind = str(data[key]['wind_string']).replace('MPH', 'km/h').replace(wmph, wkph)
+
+            bot.msg(channel, user + ': ' + curr_loc + ' - ' + temp + ' - Feels like ' \
+                                    + feel + ' - ' + weather + ' - Humidity: ' + humidity \
+                                    + ' - Wind: ' + wind)
+        elif 'results' in text:
+            bot.msg(channel, user + ': there are multiple locations matching "' + arg + '". Try to refine your search.')
+
+        elif 'querynotfound' in text:
+            bot.msg(channel, user + ': No location found.')
