c2141c309336d1e81b6f92b8f7ee28fc3023eba5
shodan <mega@ioexception.at>
2014-03-11 13:31:05 +0100

    added some logging

--- Modules/ctcp.py
+++ Modules/ctcp.py
@@ -4,4 +4,5 @@ from BotKit import handles
 
 @handles("ctcp_version")
 def version(bot, channel, user, msg):
+    bot.logger.info("%s requested version info" % user)
     bot.msg(user, "\001VERSION You are now breathing manually\001")
