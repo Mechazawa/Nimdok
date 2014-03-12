f9ca34c0e55c9d11b97cf8926df3fcf10a115e25
shodan <mega@ioexception.at>
2014-03-12 17:41:04 +0100

    Added finger Also I fixed version responses

--- Modules/ctcp.py
+++ Modules/ctcp.py
@@ -2,7 +2,14 @@
 #!/usr/bin/python
 from BotKit import handles
 
+
 @handles("ctcp_version")
 def version(bot, channel, user, msg):
     bot.logger.info("%s requested version info" % user)
-    bot.msg(user, "\001VERSION You are now breathing manually\001")
+    bot.notice(user, "\001VERSION You are now breathing manually\001")
+
+
+@handles("ctcp_finger")
+def finger(bot, channel, user, msg):
+    bot.logger.info("%s tried to finger me")
+    bot.notice(user, "\001FINGER have we met?\001")
