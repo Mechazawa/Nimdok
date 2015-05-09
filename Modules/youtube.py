70f99280369db1d3b828ecbe95a33532c559d55e
Winston Weinert <winston@ml1.net>
2015-05-08 19:48:11 -0500

    Rewrite youtube.py for the v3 api
    The v2 api was officially disabled a few days ago.
    
    In addition I found the old version didn't filter out bogus
    URLs effectively. URLs with a bad ID sometimes resulted in errors.

--- Modules/youtube.py
+++ Modules/youtube.py
@@ -1,42 +1,75 @@
 # -*- coding: utf-8 -*-
 
-import urllib2
-import json
-from re import compile
-
+import requests
+import re
+from urlparse import urlparse, parse_qs
 from BotKit import handles, stylize, humanize
+from apikeys import google
 
+YT_REGEX = re.compile(r'(?:\W|^)([A-Za-z0-9\-_]{11})(?:\W|$)')
+API_URL = ('https://www.googleapis.com/youtube/v3/videos'
+          '?part=snippet,statistics&id={vid}&key={apikey}')
 
-#ytRegex = compile(r"""(?:youtube(?:-nocookie)?\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})""")
-ytRegex = compile(r"youtu.+\W([A-Za-z0-9\-_]{11})(\W|$)")
-apiurl = "https://gdata.youtube.com/feeds/api/videos/{VID}?alt=json&v=2"
 @handles('msg')
 def parse(bot, channel, user, msg):
     for m in msg.split(' '):
-        if m[:4] == "http" and '//' in m[5:-len(m)+8]:
-            if 'youtube.' in getdomain(m) or 'youtu.be' in getdomain(m):
-                vid = ytRegex.search(m).group(1)
-                bot.logger.info("Matched youtube id: %s" % vid)
-                jo = json.loads(urllib2.urlopen(apiurl.replace("{VID}", vid)).read())
-                data = {
-                    "views" : int(jo["entry"]["yt$statistics"]["viewCount"]),
-                    "dislikes" : int(jo["entry"]["yt$rating"]["numDislikes"]) if "yt$rating" in jo["entry"] else 0,
-                    "likes" : int(jo["entry"]["yt$rating"]["numLikes"]) if "yt$rating" in jo["entry"] else 0,
-                    "author" :jo["entry"]["author"][0]["name"]["$t"],
-                    "title" :jo["entry"]["title"]["$t"]
-                }
+        parts = urlparse(m)
 
-                fmt = u"%s %s %s %s | %s views"
-                info = fmt % (
-                        stylize.Bold(stylize.Trunicate(data["title"], 60)),
-                        '| '+data["author"],
-                        stylize.SetColor(u"↑" + humanize.intcomma(data["likes"]), stylize.Color.Green),
-                        stylize.SetColor(u"↓" + humanize.intcomma(data["dislikes"]), stylize.Color.Red),
-                        humanize.intcomma(data["views"]) if data["views"] < 1000000 else humanize.intword(data["views"])
-                )
-                bot.msg(channel, info.encode('utf-8', 'ignore'))
+        if parts.scheme not in ['http', 'https']:
+            continue
 
+        if 'youtube.' in parts.netloc:
+            L = parse_qs(parts.query).get('v', '')
+            s = L[0] if L else ''
+        elif 'youtu.be' in parts.netloc:
+            s = parts.path
+        else:
+            continue
 
-def getdomain(s, nosub=False):
-    domain = s.split('/')[2].split('?')[0]
-    return domain.split('.', domain.count(".")-1)[-1] if nosub else domain
+        matches = YT_REGEX.search(s)
+        if matches is None:
+            bot.logger.info('No youtube id found.')
+            continue
+        vid = matches.group(1)
+        bot.logger.info('Matched youtube id: {}'.format(vid))
+
+        req_url = API_URL.format(
+            apikey=google,
+            vid=vid,
+        )
+
+        resp = requests.get(req_url)
+        jo = resp.json()
+        if jo['pageInfo']['totalResults'] < 1:
+            bot.logger.info('Bad youtube id: {}'.format(vid))
+            continue
+        entry = jo['items'][0]
+        stats, snippet = entry['statistics'], entry['snippet']
+
+        views = int(stats['viewCount'])
+        dislikes = int(stats['dislikeCount'])
+        likes = int(stats['likeCount'])
+        yt_channel = snippet['channelTitle']
+        title = snippet['title']
+
+        fmt = u'{title} | {channel} {likes} {dislikes} | {views} views'
+        message = fmt.format(
+            title=stylize.Bold(stylize.Trunicate(title)),
+            channel=yt_channel,
+            likes=format_likes(likes),
+            dislikes=format_dislikes(dislikes),
+            views=format_views(views),
+        ).encode('utf-8', 'ignore')
+        bot.msg(channel, message)
+
+def format_views(views):
+    fn = humanize.intcomma if views < 1000000 else humanize.intword
+    return fn(views)
+
+def format_likes(likes):
+    return stylize.SetColor(u'↑' + humanize.intcomma(likes),
+                            stylize.Color.Green)
+
+def format_dislikes(dislikes):
+    return stylize.SetColor(u'↓' + humanize.intcomma(dislikes),
+                            stylize.Color.Red)
