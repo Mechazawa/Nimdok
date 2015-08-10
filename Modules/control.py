8a88f98fdee52ebc3e4e633eeb3f7304af31c72e
shodan <mega@ioexception.at>
2015-08-10 22:52:21 +0200

    clean slate

--- Modules/control.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from BotKit import command
-
-@command('part', True)
-def parse(bot, channel, user, msg):
-    bot.part(msg.split()[0])
--- Modules/fortune.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from BotKit import command
-import os
-
-@command("fortune")
-def parse(bot, channel, user, msg):
-    bot.msg(channel, ' '.join(os.popen("fortune -a fortunes").readlines()).strip())
