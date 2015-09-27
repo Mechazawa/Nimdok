ca12a790b7375ef446fc8434af10ec3cbedaa9fc
shodan <mega@ioexception.at>
2015-09-28 00:53:06 +0200

    Eval works now

---
+++ nimdok/modules/eval.py
@@ -0,0 +1,49 @@
+from core import Module, on_command
+from bs4 import BeautifulSoup
+from core.util import truncate, threaded
+import requests
+
+
+class Eval(Module):
+
+    api_url = "https://eval.in/"
+    api_data = {'utf8': 'λ', 'execute': 'on', 'private': 'on', 'input': ''}
+
+    @threaded
+    def eval_code(self, bot, channel, user, code, language):
+        data = dict(self.api_data)
+        data['code'] = code
+        data['lang'] = language
+
+        raw = requests.post(self.api_url, data=data)
+        soup = BeautifulSoup(raw.text, 'html.parser')
+
+        response = soup.findAll('pre')[-1].text
+        response = response.rstrip().replace('\001', '')
+        if '\n' in response or '\r' in response:
+            bot.message(channel, "{user}: {paste}".format(user=user, paste=raw.url))
+        elif len(response) > 300:
+            bot.message(channel, truncate(response, 300))
+            bot.message(channel, "{user}: {paste}".format(user=user, paste=raw.url))
+        else:
+            bot.message(channel, response)
+
+    @on_command('py')
+    def eval_python3(self, *argv):
+        print(*argv)
+        self.eval_code(*argv, language='Python / CPython 3.4.1')
+
+    @on_command('py2')
+    def eval_python2(self, *argv):
+        self.eval_code(*argv, language='python/cpython-2.7.8')
+
+    @on_command('php')
+    def eval_php(self, *argv):
+        self.eval_code(*argv, language='php/php-5.5.14')
+
+    @on_command('hs')
+    def eval_haskell(self, *argv):
+        self.eval_code(*argv, language='haskell/hugs98-sep-2006')
+
+
+
