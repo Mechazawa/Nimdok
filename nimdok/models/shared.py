ccd8019ae438b8b34db0ddb4672a33621a858048
shodan <mega@ioexception.at>
2015-11-02 21:33:49 +0100

    Fixed typo

--- nimdok/models/shared.py
+++ nimdok/models/shared.py
@@ -1,9 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base as sqa_declarative_base
+from sqlalchemy.ext.declarative import declarative_base as sql_declarative_base
 
 
 class db(object):
     engine = None
     session = None
 
-declarative_base = sqa_declarative_base()
+declarative_base = sql_declarative_base()
 
