03debebd589109256635a3cb31e416313e4c9266
shodan <mega@ioexception.at>
2015-11-02 22:50:51 +0100

    Cleanup youtube module

--- nimdok/modules/youtube.py
+++ nimdok/modules/youtube.py
@@ -1,12 +1,11 @@
 from core import on_regex, Module
-from core.util import threaded
 from core.color import IrcColors
 from models import ApiKeyModel
 import requests
 
 
 class Youtube(Module):
-    _base_url = "https://www.googleapis.com/youtube/v3/videos?id={video}&key={key}&part=snippet,statistics"
+    _base_url = "https://www.googleapis.com/youtube/v3/videos"
     _template = "{title} | {channel} {green}↑{likes:,} {red}↓{dislikes:,}{reset} | {views:,} views"
 
     @on_regex(r'https?://(?:www\.)?(?:youtu\.be|youtube.\w{2,}.+)(?:\W|^)([A-Za-z0-9\-_]{11})(?:\W|$)')
@@ -18,18 +17,23 @@ class Youtube(Module):
         bot.message(channel, self._template.format(**info))
 
     def get_info(self, video):
-        url = self._base_url.format(video=video, key=self._api_key)
-        json = requests.get(url).json()
-        data = json['items'][0]
-        data_stats = data['statistics']
-        data_info = data['snippet']
+        params = {
+            'key': self._api_key,
+            'video': video,
+        }
+
+        response = requests.get(self._base_url, params=params) \
+                           .json()['items'][0]
+
+        stats = response['statistics']
+        info = response['snippet']
 
         return {
-            'channel': data_info['channelTitle'],
-            'title': data_info['title'],
-            'likes': int(data_stats['likeCount']),
-            'dislikes': int(data_stats['dislikeCount']),
-            'views': int(data_stats['viewCount']),
+            'channel': info['channelTitle'],
+            'title': info['title'],
+            'likes': int(stats['likeCount']),
+            'dislikes': int(stats['dislikeCount']),
+            'views': int(stats['viewCount']),
         }
 
     @property
