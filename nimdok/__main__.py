cea959ae0120b7830a4412907192d94a617b9f92
shodan <mega@ioexception.at>
2015-11-03 05:15:46 +0100

    Enable all modules when using --init-db

--- nimdok/__main__.py
+++ nimdok/__main__.py
@@ -1,8 +1,8 @@
 from core import nimdok
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from models import declarative_base, db
+from models import declarative_base, ModuleModel, db
 
 parser = ArgumentParser(description='Nimdok; the modular irc bot')
 
@@ -36,11 +36,15 @@ db.engine = create_engine(args.database)
 db.session = scoped_session(sessionmaker(bind=db.engine))
 declarative_base.query = db.session.query_property()
 
+
 if args.init_db is True:
     print('Creating tables')
     declarative_base.metadata.create_all(bind=db.engine)
     db.session.commit()
     print('Created tables')
+    for module in nimdok.Nimdok.list_modules():
+        ModuleModel.enable(module)
+    print('Enabled all modules')
 
 bot = nimdok.Nimdok(args.nickname, username=args.username, realname=args.realname)
 bot.connect(
